14f63045664e1648894295579f5bae2ceb674758
glitterneey <91388336+GLITTERneey@users.noreply.github.com>
2021-10-23 20:42:57 +0700

    ..

--- penangan/songs.py
+++ penangan/songs.py
@@ -51,7 +51,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Uploaded by [Hendra](t.me/IamYourEnemy)**'
+        rep = '**🎵 Uploaded by [𝓖𝓵𝓲𝓽𝓽𝓮𝓻](t.me/Biarenakliatnyaaa)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
